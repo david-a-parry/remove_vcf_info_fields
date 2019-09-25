480ef646b9c644034dd0bae313481645c7eba5e9
David A. Parry <david.parry@igmm.ed.ac.uk>
2019-09-25 15:50:33 +0100

    add option to keep fields and specify output

--- remove_info_fields.py
+++ remove_info_fields.py
@@ -1,47 +1,79 @@
 #!/usr/bin/env python3
 import sys
 import re
+import argparse
 from parse_vcf import VcfReader
-
+from Bio import bgzf
 
 info_re = re.compile(r"""\#\#INFO
                       =<ID=(\S+?)          #captures metadata ID
                       (,(.*))*             #capture remaining keys/values
                       >""",                #dict line should end with a >
                       re.X)
+def get_options():
+    parser = argparse.ArgumentParser(description='''Remove specific INFO fields
+    from a VCF file.''')
+    parser.add_argument("-i", "--vcf_input", "--vcf", required=True,
+                        help='Input VCF file')
+    parser.add_argument("-o", "--output", help='''Output VCF file. If the given
+                        filename ends with '.gz' or '.bgz' the output will be
+                        written compressed via BGZIP.''')
+    keep_or_remove = parser.add_mutually_exclusive_group(required=True)
+    keep_or_remove.add_argument('-r', '--remove_fields', nargs='+',
+                                help='One or more INFO fields to remove.')
+    keep_or_remove.add_argument('-k', '--keep_fields', nargs='+',
+                                help='''One or more INFO fields to retain. All
+                                other INFO fields will be removed as long as
+                                they are defined in the VCF header.''')
+    return parser
 
-def main(vcf_file, fields):
-    ''' Remove given INFO fields from VCF '''
-    vcf = VcfReader(vcf_file)
+def main(vcf_input, output, remove_fields=[], keep_fields=[]):
+    '''
+        Remove INFO fields from VCF.
+
+        Args:
+            vcf_input:  input VCF file
+
+            output:     VCF output file. Will write to STDOUT if not
+                        provided.
+
+            remove_fields:
+                        One or more INFO fields to remove. Can not be
+                        used in conjunction with keep_fields argument.
+
+            keep_fields:
+                        One or more INFO fields to keep. All other INFO
+                        fields defined in the VCF header will be removed.
+                        Can not be used in conjunction with remove_fields
+                        argument.
+
+    '''
+    if remove_fields and keep_fields:
+        raise RuntimeError("remove_fields and keep_fields arguments are " +
+                           "mutually exclusive.")
+    vcf = VcfReader(vcf_input)
+    if output is None:
+        vcf_writer = sys.stdout
+    elif output.endswith(('.gz', '.bgz')):
+        vcf_writer = bgzf.BgzfWriter(output)
+    else:
+        vcf_writer = open(output, 'w')
     new_head = []
+    if keep_fields:
+        remove_fields = [x for x in vcf.header.metadata['INFO'].keys() if x not
+                         in keep_fields]
     for h in vcf.header.meta_header:
         match = info_re.match(h)
-        if not match or match.group(1) not in fields:
+        if not match or match.group(1) not in remove_fields:
             new_head.append(h)
-    print("\n".join(new_head))
-    print("\t".join(vcf.col_header))
+    vcf_writer.write("\n".join(new_head) + "\n")
+    vcf_writer.write("\t".join(vcf.col_header) + "\n")
     for record in vcf:
-        record.remove_info_fields(fields)
-        print(record)
-
-usage = '''
-Usage: {} VCF field1 [field2 ... fieldN]
-
-Removes one or more INFO fields from a VCF. Input can be from STDIN if '-' is
-given as the input file.
-
-All output is written to STDOUT - you will probably want to redirect to a file
-or pipe into bgzip.
-
-Examples:
-
-    {} gnomad.genomes.r2.1.sites.vcf.bgz controls_AC controls_AF controls_AN
-
-    tabix -h gnomad.genomes.r2.1.sites.vcf.bgz 1:100000-200000 | {} - controls_AC controls_AF controls_AN
-
-'''.format(sys.argv[0], sys.argv[0], sys.argv[0])
+        record.remove_info_fields(remove_fields)
+        vcf_writer.write(str(record) + "\n")
+    vcf_writer.close()
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        sys.exit(usage)
-    main(sys.argv[1], sys.argv[2:])
+    argparser = get_options()
+    args = argparser.parse_args()
+    main(**vars(args))
